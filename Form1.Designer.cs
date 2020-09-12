e90773229670a38ce9b3c5c26a45082f9be5622b
Andrew <69423536+Lesoorub@users.noreply.github.com>
2020-09-12 21:32:21 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace OverlayServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(343, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 17;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(265, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "next screen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Magenta;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Magenta;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using Newtonsoft.Json.Linq;
+
+namespace OverlayServer
+{
+    public partial class Form1 : Form
+    {
+        public static PictureBox screen;
+        WebSocketServer wss = new WebSocketServer(25565);
+        Screen current;
+        public Form1()
+        {
+            TopMost = true;
+            InitializeComponent();
+            screen = pictureBox1;
+
+            SetScreen(Screen.PrimaryScreen);
+
+            wss.AddWebSocketService<General>("/");
+            wss.Start();
+        }
+
+        public void SetScreen(Screen scr)
+        {
+            current = scr;
+            this.StartPosition = FormStartPosition.Manual;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Location = new Point(current.Bounds.X, current.Bounds.Y);
+            this.Size = new Size(current.Bounds.Size.Width, current.Bounds.Size.Height);
+            this.ResumeLayout(false);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+        public static byte[] BitmapToByte(Bitmap bm)
+        {
+            using (var stream = new System.IO.MemoryStream())
+            {
+                bm.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return stream.ToArray();
+            }
+        }
+
+
+        public class General : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                if (e.IsText)
+                {
+                    // { draw, pos }
+                    // pen
+                    // draw = line, from = [0,0], to = [10,10]
+                    // draw = clear //clear
+                    // draw = rectangle, size = [10, 23]
+                    // draw = ellipse, rect = [1, 1]
+                    // draw = image, data = "1F45A4F..."//byte array
+                    //
+
+                    JObject json = JObject.Parse(e.Data);
+                    Pen p = Pens.Red;
+                    string draw = "none";
+                    Point pos = new Point(0, 0);
+
+                    if (json["pos"] != null)
+                        pos = json["pos"].ToPoint();
+                    if (json["draw"] != null)//{"type": "BR"}
+                        draw = json["draw"].ToString();
+                    if (json["pen"] != null)
+                        p = new Pen(System.Drawing.ColorTranslator.FromHtml(json["pen"].ToString()));
+
+                    using (Graphics g = screen.CreateGraphics())
+                        switch (draw)
+                        {
+                            case "clear":
+                                g.Clear(Color.Magenta);
+                                break;
+                            case "line":
+                                g.DrawLine(p, json["from"].ToPoint(), json["to"].ToPoint());
+                                break;
+                            case "rectangle":
+                                g.DrawRectangle(p, pos.ToRectangle(json["size"].ToPoint()));
+                                break;
+                            case "ellipse":
+                                g.DrawEllipse(p, pos.ToRectangle(json["rect"].ToPoint()));
+                                break;
+                            case "image":
+                                using (var ms = new System.IO.MemoryStream(StringToByteArray(json["data"].ToString())))
+                                using (Bitmap bm = new Bitmap(ms))
+                                    g.DrawImageUnscaledAndClipped(bm, pos.ToRectangle(bm.Size));
+                                break;
+                        }
+                }
+                else if (e.IsBinary)
+                {
+                    using (Graphics g = screen.CreateGraphics())
+                    using (var ms = new System.IO.MemoryStream(e.RawData))
+                    using (Bitmap bm = new Bitmap(ms))
+                        g.DrawImageUnscaledAndClipped(bm, new Point(0,0).ToRectangle(bm.Size));
+                }
+            }
+
+            public static string ByteArrayToString(byte[] ba)
+            {
+                return BitConverter.ToString(ba).Replace("-", "");
+            }
+            public static byte[] StringToByteArray(String hex)
+            {
+                int NumberChars = hex.Length;
+                byte[] bytes = new byte[NumberChars / 2];
+                for (int i = 0; i < NumberChars; i += 2)
+                    bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+                return bytes;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (current != null)
+            {
+                int pos = 0;
+                for (int k = 0; k < Screen.AllScreens.Length; k++)
+                    if (Screen.AllScreens[k] == current)
+                        pos = k;
+
+                if (pos + 1 < Screen.AllScreens.Length)
+                    SetScreen(Screen.AllScreens[pos + 1]);
+                else
+                    SetScreen(Screen.AllScreens[0]);
+            }
+            else
+            {
+                current = Screen.PrimaryScreen;
+            }
+        }
+    }
+}
+public static class External
+{
+    public static Point ToPoint(this JToken t) => new Point((int)t[0], (int)t[1]);
+    public static Point Add(this Point a, Point b) => new Point(a.X + b.X, a.Y + b.Y);
+    public static Point Sub(this Point a, Point b) => new Point(a.X - b.X, a.Y - b.Y);
+    public static Rectangle ToRectangle(this Point a, Point size) =>
+        new Rectangle(a.X, a.Y, size.X, size.Y);
+    public static Rectangle ToRectangle(this Point a, Size size) =>
+         new Rectangle(a.X, a.Y, size.Width, size.Height);
+}
